39a6ebaf6f0e42f202081736110d939174a0a986
Coder Emran <coderemran6910@gmail.com>
2023-11-28 03:27:22 +0600

    adde Creatior User Role Update API

--- routHandler/usersHandler.js
+++ routHandler/usersHandler.js
@@ -6,6 +6,11 @@ const UserModel = new mongoose.model('User', userScema);
 
 router.post('/', async(req, res) => {
     try {
+        const alreadyUser = await UserModel.findOne({email : req.body.email});
+        if(alreadyUser){
+            console.log("User already exists");
+            return
+        }
         const newUser = new UserModel(req.body);
         await newUser.save();
         res.send(newUser);
@@ -15,7 +20,7 @@ router.post('/', async(req, res) => {
       }
 }) 
 
-router.get('/', async(req, res) => {
+router.get('/', async(req, res) => { 
     try {
         const users = await UserModel.find();
         res.send(users);
@@ -26,7 +31,6 @@ router.get('/', async(req, res) => {
 
 router.delete('/:email', async(req, res) => {
   const email = req.params.email;
-  console.log(email);
   try {
     const user = await UserModel.findOneAndDelete(email);
     console.log("user delete success");
@@ -35,6 +39,23 @@ router.delete('/:email', async(req, res) => {
   catch (error) {
     console.error('Error fetching data:', error);
   }
+})
+
+// User role update 
+router.patch('/:email', async(req, res) => {
+    const email = req.params.email;
+    try{
+        const result = await UserModel.findOneAndUpdate(
+            { email: email },
+            { role: 'creator' },
+            { new: true }
+          );
+          console.log("User role update success", result);
+          res.send(result);
+    }
+    catch(error){
+        console.error('Error fetching data:', error);
+    }
 })
 
 module.exports = router
--- schemas/usersSchema.js
+++ schemas/usersSchema.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     name : {type : String, required : true},
     email: {type : String, required : true},
-    image: {type : String}
+    image: {type : String},
+    role: {type : String, enum : ['admin', 'user'], default : 'user'}
 }) 
 
 module.exports = userSchema
