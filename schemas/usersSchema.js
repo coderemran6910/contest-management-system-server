24e98a31a3c8d5a81100f4aca8f800694c361059
Coder Emran <coderemran6910@gmail.com>
2023-11-27 22:50:29 +0600

    update users get API

--- schemas/usersSchema.js
+++ schemas/usersSchema.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     name : {type : String, required : true},
-    email: {type : String}
+    email: {type : String, required : true},
+    image: {type : String}
 }) 
 
 module.exports = userSchema
